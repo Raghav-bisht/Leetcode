866adf6ae70d24fc030e7cff70a41a07dba935e9
Raghav Bisht <raghavbisht555@gmail.com>
2026-06-30 12:33:11 +0530

    Time: 12 ms (8.39%), Space: 37.3 MB (6.61%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string ans = "";
+
+        while (ss >> word) {
+            if (ans!="")
+                ans = " " + ans;
+
+            ans = word + ans;
+        }
+
+        return ans;
+    }
+};
